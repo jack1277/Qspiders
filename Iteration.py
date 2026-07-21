00832096630094fb5680e652732f1600c68e6e91
P JACOB GODSON <jgodson10@gmail.com>
2026-07-21 10:12:52 +0530

    FOR LOOP

---
+++ Iteration.py
@@ -0,0 +1,26 @@
+""" 
+Program for using FOR loop in List 
+l=[1,2,3,'s','k',3.2]
+for i in l:
+    print(i)
+
+"""
+"""
+Program for using FOR loop in tuple
+t=('c','python','c','Java')
+for i in t:
+    print(i)
+"""
+"""
+Program for using FOR loop in Set
+s={'a','b',3,4,5,8,8,5}
+for i in s:
+    print(i)
+
+"""
+"""
+Program for using FOR loop in Dictionary
+d={'a':1,'b':2,'c':3}
+for i in d:
+    print(d[i])
+  """  
